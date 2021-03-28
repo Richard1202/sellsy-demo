a008cf6915f5ecf8376b793375bcaf8848c39bcd
Pedro Alonso <pedro@unifeyed.com>
2021-03-28 04:16:19 -0400

    Integrate fingerprint

---
+++ app/src/main/java/com/example/demo/helper/PrefsHelper.kt
@@ -0,0 +1,52 @@
+package com.example.demo.helper
+
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+
+class PrefsHelper(private val sp: SharedPreferences) {
+
+    companion object {
+        // Logged in user name
+        const val PREF_USER_NAME = "pref_user_name"
+        // Logged in user password
+        const val PREF_USER_PASSWORD = "pref_user_password"
+
+        fun shared(context: Context): PrefsHelper {
+            val sharedPreferences: SharedPreferences = context.getSharedPreferences("sellsy",Context.MODE_PRIVATE)
+            return PrefsHelper(sharedPreferences)
+        }
+
+    }
+
+    var userName: String?
+        get() {
+            val value = sp.getString(PREF_USER_NAME, "")
+            return if (value != null) value else null
+        }
+        set(value) {
+            sp.edit().apply {
+                if (value == null) {
+                    remove(PREF_USER_NAME)
+                } else {
+                    putString(PREF_USER_NAME, value)
+                }
+            }.apply()
+        }
+
+    var password: String?
+        get() {
+            val value = sp.getString(PREF_USER_PASSWORD, "")
+            return if (value != null) value else null
+        }
+        set(value) {
+            sp.edit().apply {
+                if (value == null) {
+                    remove(PREF_USER_PASSWORD)
+                } else {
+                    putString(PREF_USER_PASSWORD, value)
+                }
+            }.apply()
+        }
+}
--- app/src/main/java/com/example/demo/ui/view/LoginActivity.kt
+++ app/src/main/java/com/example/demo/ui/view/LoginActivity.kt
@@ -2,14 +2,25 @@ package com.example.demo.ui.view
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import com.example.demo.R
+import com.example.demo.helper.PrefsHelper
 import kotlinx.android.synthetic.main.activity_login.*
+import java.util.concurrent.Executor
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var context: Context
+
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +28,53 @@ class LoginActivity : AppCompatActivity() {
 
         context = this@LoginActivity
 
+        username.setText(PrefsHelper.shared(this).userName)
+        password.setText(PrefsHelper.shared(this).password)
+
+        executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int,
+                                                   errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(applicationContext,
+                        "Authentication error: $errString", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    val nextScreenIntent = Intent(this@LoginActivity, MainActivity::class.java)
+                    startActivity(nextScreenIntent)
+                    finish()
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(applicationContext, "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Use account password")
+            .build()
+
         btn_login.setOnClickListener {
+
+            PrefsHelper.shared(this).userName = username.text.toString()
+            PrefsHelper.shared(this).password = password.text.toString()
+
             val nextScreenIntent = Intent(this, MainActivity::class.java)
             startActivity(nextScreenIntent)
             finish()
+        }
+
+        btn_fingerprint.setOnClickListener {
+            biometricPrompt.authenticate(promptInfo)
         }
 
     }
